5e6abeb757d644d8b441cbb2023badf3ba7f540d
Jack Winstanley-Punch <jackwp1996@gmail.com>
2020-07-22 12:32:58 +0100

    Baby steps

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    home: Home(),
-  ));
+      home: Home(),
+    ));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
           title: Text("my first app"),
           centerTitle: true,
-          backgroundColor:  Colors.deepPurpleAccent[200]
+          backgroundColor: Colors.deepPurpleAccent[200]),
+      body: Center(
+        child: Icon(
+          Icons.add_alarm,
+          color: Colors.cyanAccent[400],
         ),
-        body: Center(
-          child: Text(
-              "Shift hours app",
-              style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2,
-              color: Colors.grey[200]
-          ),
-        ),
-    ),
-    floatingActionButton: FloatingActionButton(
-    onPressed: () {},
-    child: Text("add hours"),
-    backgroundColor:  Colors.deepPurpleAccent[200],
-    ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text("add hours"),
+        backgroundColor: Colors.deepPurpleAccent[200],
+      ),
     );
   }
 }
-
